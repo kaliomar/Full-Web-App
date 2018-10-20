058ae4b45334b46f082874ab1cbdb7699da1e2a4
kevin omar <kaliomar89@yahoo.com>
2018-10-20 03:24:01 +0200

    edit listenning function

--- app.js
+++ app.js
@@ -44,6 +44,6 @@ app.use("/members/:id/comments", commentsRoutes);
 app.use("/members", memberRoutes);
 
 
-app.listen(3000, function() {
-	console.log("Start Listening on Port 3000 ....");
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+  console.log("Start listenning ...");
 });
